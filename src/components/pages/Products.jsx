e097eba6a93e2969a77a63cc4f673b913f4fcfbc
Nuris2 <nurislam.murzaliev01@gmail.com>
2022-06-20 19:30:43 +0600

    Baike

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -17,7 +17,7 @@ import ReactPaginate from "react-paginate";
 const Products = () => {
   const { getProducts, products, deleteProduct } = useContext(productContext);
 
-  const { addProductToCart } = useContext(cartContext);
+  //   const { addProductToCart } = useContext(cartContext);
 
   const [searchParams, setSearchParams] = useSearchParams();
   // Хук useSearchParams предназначен для чтения и изменения строки запроса в URL для текущего маршрута. По аналогии с хуком useState возвращает значение и функцию для изменения этого значения.
@@ -152,9 +152,9 @@ const Products = () => {
                     </Button>
                   </NavLink>
 
-                  <Button onClick={(e) => addProductToCart(item)}>
+                  {/* <Button onClick={(e) => addProductToCart(item)}>
                     <AddShoppingCartIcon />
-                  </Button>
+                  </Button> */}
                 </CardActions>
               </Card>
             ))
